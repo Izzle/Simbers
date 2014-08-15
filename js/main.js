76bd5ce37fa07d1c5b7748270e532dd1a4017f6b
Ron Martin <ronmartin89@gmail.com>
2014-08-15 15:21:53 -0700

    Checks if User has JS enabled

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@
  * Main JS functionality
  * ------------------------------------------------------------------------------
  */
+
+//Self Invoking Anonymous Function
+$(document).ready(function() {
+
+$('html').addClass('js'); //Checks if User has Javascript enabled. If they don't no class is added.
+
+});
